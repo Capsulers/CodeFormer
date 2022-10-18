6ec0adb8b8ef46ea39c2ff1adb5cab2a94a4e546
Capsulers <113909207+Capsulers@users.noreply.github.com>
2022-10-19 02:21:34 +0800

    Update inference_codeformer.py

--- inference_codeformer.py
+++ inference_codeformer.py
@@ -228,7 +228,7 @@ if __name__ == '__main__':
         if not args.has_aligned and restored_img is not None:
             if args.suffix is not None:
                 basename = f'{basename}_{args.suffix}'
-            save_restore_path = os.path.join(result_root, 'final_results', f'{basename}.png')
+            save_restore_path = os.path.join(result_root, f'{basename}.jpeg')
             imwrite(restored_img, save_restore_path)
 
     # save enhanced video
